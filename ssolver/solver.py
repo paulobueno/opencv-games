78068aa6e47c5769c0067657cde9b5b5c2c95c6e
Paulo Bueno <paulob.bruno@gmail.com>
2021-08-09 00:37:50 -0300

    refactoring

--- ssolver/solver.py
+++ ssolver/solver.py
@@ -3,45 +3,79 @@ from collections import Counter
 
 import numpy as np
 
-square = np.zeros((3, 3))
-line = [square for _ in range(3)]
-game = [line for _ in range(3)]
-game = np.asarray(game)
-game[0, 0] = [[0, 2, 1], [0, 3, 5], [0, 4, 8]]
-game[0, 1] = [[4, 7, 0], [6, 1, 8], [0, 0, 9]]
-game[0, 2] = [[5, 9, 8], [7, 2, 0], [0, 0, 0]]
-game[1, 0] = [[1, 0, 9], [2, 0, 0], [0, 0, 7]]
-game[1, 1] = [[0, 0, 0], [1, 0, 0], [0, 0, 6]]
-game[1, 2] = [[4, 7, 0], [8, 0, 3], [0, 0, 0]]
-game[2, 0] = [[8, 0, 0], [0, 0, 2], [0, 0, 0]]
-game[2, 1] = [[0, 0, 0], [7, 3, 0], [0, 6, 2]]
-game[2, 2] = [[0, 3, 0], [0, 0, 0], [9, 0, 7]]
 
-
-def print_sudoku(game_array):
-    template = '|' + 3 * '{:^3.0f}'
-    row_template = 3 * template
-    line_template = '+' + 9 * '-'
-    print(3 * line_template + '+')
-    for i in range(3):
-        for j in range(3):
-            row = game_array[i, :, j, :].flatten()
-            print(row_template.format(*row) + '|')
-        print(3 * line_template + '+')
-    return None
+def get_all_opts():
+    keys = list(it.product(*4 * [[0, 1, 2]]))
+    values = list(range(1, 10))
+    opts = {key: values for key in keys}
+    return opts
 
 
 def check_missing(array):
+    """
+    Check missing numbers of an array.
+    The array can be a row, column or a square,
+    where each one has to have 1 to 9, with no duplicated number.
+    :param array:
+    :return: list of missing numbers
+    """
     look_values = range(1, 10)
     remain_values = [value for value in look_values if
                      value not in array.flatten()]
     return remain_values
 
 
+def mockup_game():
+    square = np.zeros((3, 3))
+    line = [square for _ in range(3)]
+    game = [line for _ in range(3)]
+    game = np.asarray(game)
+    game[0, 0] = [[0, 2, 1], [0, 3, 5], [0, 4, 8]]
+    game[0, 1] = [[4, 7, 0], [6, 1, 8], [0, 0, 9]]
+    game[0, 2] = [[5, 9, 8], [7, 2, 0], [0, 0, 0]]
+    game[1, 0] = [[1, 0, 9], [2, 0, 0], [0, 0, 7]]
+    game[1, 1] = [[0, 0, 0], [1, 0, 0], [0, 0, 6]]
+    game[1, 2] = [[4, 7, 0], [8, 0, 3], [0, 0, 0]]
+    game[2, 0] = [[8, 0, 0], [0, 0, 2], [0, 0, 0]]
+    game[2, 1] = [[0, 0, 0], [7, 3, 0], [0, 6, 2]]
+    game[2, 2] = [[0, 3, 0], [0, 0, 0], [9, 0, 7]]
+    return game
+
+
+class Sudoku:
+
+    def __init__(self, game=None):
+        if not game:
+            game = mockup_game()
+        self.game = game
+        self.options = get_all_opts()
+
+    def print_sudoku(self):
+        """
+        Print sudoku game as a string in the terminal
+        """
+        template = '|' + 3 * '{:^3.0f}'
+        row_template = 3 * template
+        line_template = '+' + 9 * '-'
+        print(3 * line_template + '+')
+        for i in range(3):
+            for j in range(3):
+                row = self.game[i, :, j, :].flatten()
+                print(row_template.format(*row) + '|')
+            print(3 * line_template + '+')
+        return None
+
+    def check_missing_row(self, i, ii):
+        return check_missing(self.game[i, :, ii, :])
+
+    def check_missing_column(self, j, jj):
+        return check_missing(self.game[:, j, :, jj])
+
+    def check_missing_square(self, i, j):
+        return check_missing(self.game[i, j, :, :])
+
+
 def poss_dict(game):
-    keys = list(it.product(*4 * [[0, 1, 2]]))
-    values = list(range(1, 10))
-    poss = {key: values for key in keys}
     for location in poss.keys():
         i, j, ii, jj = location
         if game[location] != 0:
@@ -61,7 +95,3 @@ def fill_game(game):
         if len(v) == 1 and game[k] == 0:
             game[k] = v[0]
     return game
-
-
-game = fill_game(game)
-print_sudoku(game)
